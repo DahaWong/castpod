29ac083eb7155680761c48fc059a50f26f540d42
DahaWong <dahawong@gmail.com>
2021-01-01 21:29:58 +0800

    Fix bugs.

--- bot.py
+++ bot.py
@@ -23,11 +23,11 @@ print(dispatcher.user_data)
 register(updater.dispatcher)
 
 # Polling:
-# updater.start_polling()
-# updater.idle()
+updater.start_polling()
+updater.idle()
 
 # Webhook:
-updater.start_webhook(**webhook_info)
-updater.bot.set_webhook(**webhook_setting)
+# updater.start_webhook(**webhook_info)
+# updater.bot.set_webhook(**webhook_setting)
 
 # set_jobs(updater.job_queue)
