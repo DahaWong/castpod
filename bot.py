fcddaa2ad18778cea6be794ed2e3b6f0f175eb86
DahaWong <dahawong@gmail.com>
2021-01-03 09:34:01 +0800

    test

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from telegram.ext import Updater
 from handlers.register import register
 from utils.persistence import persistence
 from utils.schedule import set_jobs
-import sys
-sys.setdefaultencoding('utf-8')
 
 updater = Updater(**update_info)
 dispatcher = updater.dispatcher
--- utils/parser.py
+++ utils/parser.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 def parse_opml(f):
     feeds = []
     soup = BeautifulSoup(f, 'xml')
+    print('ok')
     for podcast in soup.find_all(type="rss"):
         _, name, url = podcast.attrs.values()
         feeds.append({"name":name, "url":url})
