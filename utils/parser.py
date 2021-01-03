d2a3d8be11c26c54f05d7ca589b3e03e40d64dcf
DahaWong <dahawong@gmail.com>
2021-01-03 16:53:25 +0800

    Debug

--- utils/parser.py
+++ utils/parser.py
@@ -1,9 +1,15 @@
 import feedparser
 from bs4 import BeautifulSoup
 
+import sys
+defaultencoding = 'utf-8'
+if sys.getdefaultencoding() != defaultencoding:
+    reload(sys)
+    sys.setdefaultencoding(defaultencoding)
+
 def parse_opml(f):
     feeds = []
-    soup = BeautifulSoup(f, 'lxml', from_encoding="utf8")
+    soup = BeautifulSoup(f, 'lxml', from_encoding="utf-8")
     print(soup.original_encoding)
     print(soup)
     for podcast in soup.find_all(type="rss"):
