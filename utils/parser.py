680065c760cb3bbd6859ec3f4a7b03c6e98b7ac7
DahaWong <dahawong@gmail.com>
2021-01-03 16:13:54 +0800

    Debug

--- utils/parser.py
+++ utils/parser.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 def parse_opml(f):
     feeds = []
     soup = BeautifulSoup(f, 'xml')
+    print('1')
     for podcast in soup.find_all(type="rss"):
+        print('2')
         _, name, url = podcast.attrs.values()
+        print('3')
         feeds.append({"name":name, "url":url})
     return feeds
