55f55abea6484ae4069406e8b9babc393df9821c
DahaWong <dahawong@gmail.com>
2021-01-04 09:13:42 +0800

    Test progress bar.

--- utils/downloader.py
+++ utils/downloader.py
@@ -4,17 +4,11 @@ import requests
 
 def local_download(url, chat_id):
     res = requests.get(url, allow_redirects=True, stream=True)
-    total_size_in_bytes= int(res.headers.get('content-length', 0))
-    print(total_size_in_bytes)
+    total = int(res.headers.get('content-length', 0))
+    print(total)
     if res.status_code != 200: 
         raise Exception("Error when downloading file.")
-    progress_bar = tqdm(
-        total = total_size_in_bytes, 
-        unit='it', 
-        unit_scale=True,
-        token = bot_token,
-        chat = chat_id
-        )
+    progress_bar = tqdm(total = total, unit='it', unit_scale=total,token = bot_token,chat = chat_id)
     print(progress_bar)
     file_path = f'public/audio/audio-temp.mp3'
     with open(file_path, 'wb') as f:
