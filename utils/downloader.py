f06106317d631e31cbd8bbb8c0b9b9475acd794c
DahaWong <dahawong@gmail.com>
2021-01-04 10:40:23 +0800

    Test progress bar.

--- utils/downloader.py
+++ utils/downloader.py
@@ -8,17 +8,22 @@ def local_download(url, chat_id):
     block_size = 1024 #1 Kibibyte
     if res.status_code != 200: raise Exception("Error when downloading file.")
     progress_bar = tqdm(
+        desc= "正在下载节目…"
         total = total, 
         unit='iB', 
         unit_scale=True,
         token = bot_token,
         chat_id = chat_id,
-        bar_format= '{percentage:3.0f}%  {bar:10} {remaining}'
+        bar_format= '{percentage:3.0f}%  {bar:8} {post_fix}',
+        postfix= '\n{remaining_s}秒后下载完成'
     )
     with open('public/audio/audio-temp.mp3', 'wb') as f:
         for data in res.iter_content(block_size):
             progress_bar.update(len(data))
             f.write(data)
+        progress_bar.set_postfix('')
+        progress_bar.set_description('下载完毕！')
+        progress_bar.disable=True
     progress_bar.close()
     if total != 0 and progress_bar.n != total:
         raise Exception("ERROR, something went wrong with progress bar.")
