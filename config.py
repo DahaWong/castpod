b7c5a09fbd8ee6289d30806054e34b7103497db4
DahaWong <dahawong@gmail.com>
2021-01-04 17:29:40 +0800

    Try downloading large audio.

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ webhook_info = {
 webhook_setting = {
     "url": f'http://webhook.daha.me/{bot_token}',
     "max_connections": 1000,
-    drop_pending_updates: True
+    "drop_pending_updates": True
     # "allowed_updates": []
 }
  
