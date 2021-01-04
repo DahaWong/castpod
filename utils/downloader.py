d3f3663ee0f6cb3f95e4fd6132619274c0d4d980
DahaWong <dahawong@gmail.com>
2021-01-04 09:38:58 +0800

    Test progress bar.

--- utils/downloader.py
+++ utils/downloader.py
@@ -17,7 +17,7 @@ def local_download(url, chat_id):
     with open('public/audio/audio-temp.mp3', 'wb') as f:
         for data in res.iter_content(block_size):
             progress_bar.update(len(data))
-            file.write(data)
+            f.write(data)
     progress_bar.close()
     if total != 0 and progress_bar.n != total:
         raise Exception("ERROR, something went wrong with progress bar.")
