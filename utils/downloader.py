4577720d600a1f21fe33401d93de4cb033415a6f
DahaWong <dahawong@gmail.com>
2021-01-04 09:38:08 +0800

    Test progress bar.

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,15 +1,18 @@
-from tqdm import tqdm
+from tqdm.contrib.telegram import tqdm
 from config import bot_token
 import requests
 
 def local_download(url, chat_id):
     res = requests.get(url, allow_redirects=True, stream=True)
     total = int(res.headers.get('content-length', 0))
+    block_size = 1024 #1 Kibibyte
     if res.status_code != 200: raise Exception("Error when downloading file.")
     progress_bar = tqdm(
         total = total, 
         unit='iB', 
-        unit_scale=True
+        unit_scale=True,
+        token = bot_token,
+        chat_id = chat_id
     )
     with open('public/audio/audio-temp.mp3', 'wb') as f:
         for data in res.iter_content(block_size):
