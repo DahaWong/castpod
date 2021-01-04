72183a7eb5de42be826ed6e46a00fd3c86d26fb7
DahaWong <dahawong@gmail.com>
2021-01-04 16:36:46 +0800

    Try downloading large audio.

--- models.py
+++ models.py
@@ -74,7 +74,7 @@ class Episode(object):
         self.audio = self.set_audio(episode.enclosures)
         if self.audio:
             self.audio_url = self.audio.href
-            self.audio_size = self.audio.get('length')
+            self.audio_size = self.audio.get('length') or ''
         else:
             self.audio_url = ""
             self.audio_size = 0
