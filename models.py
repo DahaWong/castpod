f31be8edecf0c1f5a006307da98fe7c23d62aede
DahaWong <dahawong@gmail.com>
2021-01-04 14:33:42 +0800

    Download run async.

--- models.py
+++ models.py
@@ -74,7 +74,7 @@ class Episode(object):
         self.audio = self.set_audio(episode.enclosures)
         if self.audio:
             self.audio_url = self.audio.href
-            self.audio_size = self.audio.get('length') or 0
+            self.audio_size = self.audio.get('length')
         else:
             self.audio_url = ""
             self.audio_size = 0
