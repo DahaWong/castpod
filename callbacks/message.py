c56c0deac00ff814cf03411da95aa8328b903caf
DahaWong <dahawong@gmail.com>
2021-01-12 10:41:19 +0800

    Async import feeds.

--- callbacks/message.py
+++ callbacks/message.py
@@ -42,6 +42,7 @@ def save_subscription(update, context):
             except Exception as e: 
                 print(e)
                 failed_feeds.append(feed['url'])
+                return
         else:
             podcast = cached_podcasts[feed['name']]
             podcasts.append(podcast)
