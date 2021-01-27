b4eec6237a87892746dbffaa9ea282d29b1343b6
DahaWong <dahawong@gmail.com>
2021-01-27 17:33:02 +0800

    Debug.

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ updater = Updater(**update_info)
 dispatcher = updater.dispatcher
 
 # Use this method to logout your bot from telegram api cloud:
-updater.bot.log_out()
+# updater.bot.log_out()
 
 # Use these methods before you move your bot to another local server:
-updater.bot.delete_webhook() 
-updater.bot.close()
+# updater.bot.delete_webhook() 
+# updater.bot.close()
 
 # Polling:
 # updater.start_polling()
