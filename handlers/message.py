4ba285e421e6adf75f15bf8688cd62a9dc4b17da
DahaWong <dahawong@gmail.com>
2021-01-30 21:19:57 +0800

    Refactor.

--- handlers/message.py
+++ handlers/message.py
@@ -1,18 +1,20 @@
 from telegram.ext import MessageHandler, Filters
 import callbacks.message as callback
 
-subscription_handler = MessageHandler(
-    Filters.document.mime_type('text/xml') | Filters.document.file_extension(
-        "opml") | Filters.document.file_extension("opm"),
-    callback.save_subscription
-)
-
 handlers = [
     MessageHandler(
         Filters.entity("url") & Filters.regex(r'^https?://'), callback.subscribe_feed),
     MessageHandler(
         Filters.regex(r'🎙️ (.+) #([0-9]+)'), callback.download_episode),
-    MessageHandler(Filters.regex(r'^╳$') | Filters.regex(r'^订阅列表是空的～$'), callback.exit_reply_keyboard, run_async=True),
+    MessageHandler(
+        Filters.regex(r'^╳$') |
+        Filters.regex(r'^订阅列表是空的～$'), callback.exit_reply_keyboard, run_async=True),
+    MessageHandler(
+        Filters.document.mime_type('text/xml') |
+        Filters.document.file_extension("opml") |
+        Filters.document.file_extension("opm"),
+        callback.save_subscription
+    ),
     MessageHandler(Filters.text, callback.show_feed),
     MessageHandler(Filters.audio, callback.handle_audio)
 ]
