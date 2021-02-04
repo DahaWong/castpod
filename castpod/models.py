0e50178b6fa4433682e9c8b793931ce189bdd627
DahaWong <dahawong@gmail.com>
2021-02-05 00:26:17 +0800

    Connect MongoDB.

--- castpod/models.py
+++ castpod/models.py
@@ -152,7 +152,7 @@ class Podcast(Document):
     email = StringField()
     episodes = EmbeddedDocumentListField(Episode)
     subscribers = ListField(ReferenceField(User, reverse_delete_rule=PULL))
-    update_time = DateTimeField()
+    # update_time = DateTimeField()
     job_group = ListField(IntField(min_value=0, max_value=47))
 
     @classmethod
