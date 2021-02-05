eb66f173ec3b12bdfbefb07a8e8970e7100b646e
DahaWong <dahawong@gmail.com>
2021-02-05 11:24:17 +0800

    Connect MongoDB.

--- castpod/stats.py
+++ castpod/stats.py
@@ -1,17 +1,17 @@
-from ptbstats import set_dispatcher, register_stats, SimpleStats
-from telegram.ext import Filters
+# from ptbstats import set_dispatcher, register_stats, SimpleStats
+# from telegram.ext import Filters
 
 
-def register(dispatcher):
-    set_dispatcher(dispatcher)
+# def register(dispatcher):
+#     set_dispatcher(dispatcher)
 
-    register_stats(
-        SimpleStats(
-            'text',
-            lambda u: bool(u.message and (Filters.text & ~ Filters.command)(u))
-        )
-    )
-    register_stats(
-        SimpleStats(
-            'inline', lambda u: bool(u.inline_query and u.inline_query.query))
-    )
+#     register_stats(
+#         SimpleStats(
+#             'text',
+#             lambda u: bool(u.message and (Filters.text & ~ Filters.command)(u))
+#         )
+#     )
+#     register_stats(
+#         SimpleStats(
+#             'inline', lambda u: bool(u.inline_query and u.inline_query.query))
+#     )
