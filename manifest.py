d0dc9c34985b51a275e1d90693bb3af3832bc6f9
Daha <dahawong@gmail.com>
2021-03-01 18:23:57 +0800

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -3,7 +3,7 @@ class manifest:
   bot_id = "castpodbot"
   author= "Daha"
   author_url = "https://office.daha.me/"
-  version= "0.0.1"
+  version= "0.1.1"
   discription=""
   author_id = "dahawong"
   repo = "https://github.com/dahawong/castpod"
